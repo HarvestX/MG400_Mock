a8960c8e9067592743f1d5b5587b65635e17f4bf
ss-koishi <yuuki100201@gmail.com>
2022-03-17 23:04:53 +0900

    port configuration

--- app/src/main.py
+++ app/src/main.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+@app.route('/do_2999')
+def do_2999():
+    return '2999'
+
+@app.route('/do_30003')
+def do_30003():
+    return '30003'
 
 if __name__ == "__main__":
     app.run()
