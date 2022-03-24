7b4212d5a05bc741ae7f52630be1a771d2a4649b
ss-koishi <yuuki100201@gmail.com>
2022-03-24 22:14:55 +0900

    listen 29999, 30003

---
+++ app/src/dobot_server.py
@@ -0,0 +1,26 @@
+import socket
+from threading import Thread
+
+class DobotServer(Thread):
+
+    MAX_RECEIVE_BYTES = 1024
+
+    socket_info = None
+
+    def __init__(self, socket_info):
+        self.socket_info = socket_info
+        super().__init__()
+
+    def run(self):
+        thread = Thread(target=self.__execute)
+        thread.run()
+
+    def __execute(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((self.socket_info[0], self.socket_info[1]))  # IPとポート番号を指定
+        s.listen(10)
+
+        while True:
+            connection, _ = s.accept()
+            test = connection.recv(self.MAX_RECEIVE_BYTES).decode()
+            connection.send((test + ' returned.').encode())
--- app/src/main.py
+++ app/src/main.py
@@ -1,14 +1,12 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/do_2999')
-def do_2999():
-    return '2999'
-
-@app.route('/do_30003')
-def do_30003():
-    return '30003'
+from dobot_server import DobotServer
 
 if __name__ == "__main__":
-    app.run()
+
+    required_socket_list = [
+        [ '172.10.0.3', 29999 ],
+        [ '172.10.0.3', 30003 ],
+    ]
+
+    for socket in required_socket_list:
+        server = DobotServer(socket)
+        server.start()
