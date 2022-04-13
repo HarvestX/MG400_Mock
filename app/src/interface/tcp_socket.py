bb312f9ca5f49028abedc873b1568e25865ff53d
ss-koishi <yuuki100201@gmail.com>
2022-04-13 22:15:59 +0900

    TcpSocket members' private

--- app/src/interface/tcp_socket.py
+++ app/src/interface/tcp_socket.py
@@ -9,9 +9,9 @@ class TcpSocket(Thread):
 
     MAX_RECEIVE_BYTES = 1024
 
-    ip: str = None
-    port: int = None
-    logger: logging.Logger = None
+    __ip: str = None
+    __port: int = None
+    __callback = None
 
     @abstractmethod
     def callback(self, socket, max_receive_bytes):
@@ -19,10 +19,12 @@ class TcpSocket(Thread):
 
     def __init__(self, ip: str, port: int, callback) -> None:
         super().__init__()
+        self.__ip = ip
+        self.__port = port
         self.__callback = callback
 
     def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((self._ip, self._port))
+            s.bind((self.__ip, self.__port))
             s.listen(10)
             self.__callback(s, self.MAX_RECEIVE_BYTES)
