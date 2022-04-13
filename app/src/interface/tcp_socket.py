4eb9316a8979f8323ad225be62b90d1f693111f6
ss-koishi <yuuki100201@gmail.com>
2022-04-13 22:15:17 +0900

    TcpSocket can define the behavior by dependency injection

--- app/src/interface/tcp_socket.py
+++ app/src/interface/tcp_socket.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 import socket
 from threading import Thread
 
@@ -12,22 +13,16 @@ class TcpSocket(Thread):
     port: int = None
     logger: logging.Logger = None
 
-    def __init__(self, name: str, ip: str, port: int) -> None:
+    @abstractmethod
+    def callback(self, socket, max_receive_bytes):
+        pass
+
+    def __init__(self, ip: str, port: int, callback) -> None:
         super().__init__()
-        self._ip = ip
-        self._port = port
-        self.logger = logging.getLogger(name)
+        self.__callback = callback
 
     def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self._ip, self._port))
             s.listen(10)
-            connection, _ = s.accept()
-            with connection:
-                while True:
-                    recv = connection.recv(self.MAX_RECEIVE_BYTES).decode()
-                    if not recv:
-                        continue
-
-                    self.logger.info(recv)
-                    connection.send((recv + ' returned.').encode())
+            self.__callback(s, self.MAX_RECEIVE_BYTES)
