2898039e2d31a482b9ce5079370211c3c3342fae
ss-koishi <yuuki100201@gmail.com>
2022-04-29 00:46:16 +0900

    translate string of command to call the method by specifying the string

---
+++ app/src/interface/function_parser.py
@@ -0,0 +1,21 @@
+import re as regex
+
+class FunctionParesr:
+
+    def exec(self, command: str):
+        function_name = regex.match(r'\s*[a-zA-Z]+(?=(\s*\(.*\)))', command)
+
+        if not function_name:
+            raise ValueError('Invalid command: ' + command)
+
+        function_name_str = function_name.group()
+
+        args_str = command.replace(function_name_str, '').strip().lstrip('(').rstrip(')')
+        args = args_str.split(',')
+
+        try:
+            function = getattr('functions_class', function_name_str)
+
+            function(args)
+        except AttributeError as err:
+            raise ValueError('Not found a function: ' + err)
