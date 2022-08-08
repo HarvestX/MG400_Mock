88eb44f3848b27297862070fc9dccac0beb5b867
Yuki Yamamoto <yuki8mamo10.hu@gmail.com>
2022-08-08 23:35:45 +0900

    implement EnableRobot

--- app/src/dobot_command/dashboard_command.py
+++ app/src/dobot_command/dashboard_command.py
@@ -1,6 +1,6 @@
 """Dobot DashBoard Commands."""
 
-from dobot_command.dobot_hardware import DobotHardware
+from dobot_command.dobot_hardware import DobotHardware, RobotMode
 
 
 class DashboardCommands:
@@ -8,16 +8,26 @@ class DashboardCommands:
 
     def __init__(self, dobot: DobotHardware) -> None:
         self.__dobot = dobot
+        self.none = "None"
 
-    def EnableRobot(self):
+    def EnableRobot(self, **kwargs) -> str:
         """EnableRobot"""
+        error_id = self.__dobot.get_error_id()
+        if error_id == 0:
+            self.__dobot.set_robot_mode(RobotMode().mode_init)
+            return str(error_id)
+        else:
+            return str(error_id)
 
-    def DisableRobot(self):
+    def DisableRobot(self) -> str:
         """DisableRobot"""
+        return self.none
 
-    def ClearError(self):
+    def ClearError(self) -> str:
         """ClearError"""
         self.__dobot.clear_error()
+        return self.none
 
-    def GetErrorID(self):
+    def GetErrorID(self) -> str:
         """GetErrorID"""
+        return self.none
