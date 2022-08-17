78dd6fae633035a18092f66a2814930e28e335e0
Yuki Yamamoto <yuki8mamo10.hu@gmail.com>
2022-08-17 23:22:34 +0900

    add remaining dashboard commands

--- app/src/dobot_command/motion_command.py
+++ app/src/dobot_command/motion_command.py
@@ -21,7 +21,10 @@ class MotionCommands:
             return False
 
         tool_target = list(map(float, args[0:6]))
-        self.__dobot.set_tool_vector_target(tool_target)
+        if not self.__dobot.set_tool_vector_target(tool_target):
+            self.__dobot.log_warning_msg("The number of arguments is invalid.")
+            return False
+
         self.__dobot.register_init_status()
         accepted = self.__dobot.generate_target_in_joint()
         if accepted:
@@ -66,7 +69,10 @@ class MotionCommands:
             return False
         tool_target = list(map(float, args[0:6]))
 
-        self.__dobot.set_tool_vector_target(tool_target)
+        if not self.__dobot.set_tool_vector_target(tool_target):
+            self.__dobot.log_warning_msg("The number of arguments is invalid.")
+            return False
+
         self.__dobot.register_init_status()
         accepted = self.__dobot.generate_target_in_tool()
         if accepted:
@@ -80,6 +86,26 @@ class MotionCommands:
 
     def JointMovJ(self, args):
         """JointMovJ"""
-        _ = args  # for pylint waring
-        self.__dobot.log_warning_msg(
-            "The JointMovJ command has not yet been implemented.")
+        # TODO: to be acceptable optional args.
+        if self.__dobot.get_robot_mode() is not robot_mode.MODE_ENABLE:
+            self.__dobot.log_warning_msg("The robot mode is not enable.")
+            return False
+        if len(args) < 6:
+            self.__dobot.log_warning_msg("The number of arguments is invalid.")
+            return False
+
+        q_target = list(map(float, args[0:6]))
+        if not self.__dobot.set_q_target(q_target):
+            self.__dobot.log_warning_msg("The target is invalid.")
+            return False
+
+        self.__dobot.register_init_status()
+        accepted = self.__dobot.generate_target_in_joint()
+        if accepted:
+            self.__dobot.set_robot_mode(robot_mode.MODE_RUNNING)
+            self.__dobot.reset_time_index()
+            self.__dobot.log_info_msg("The dobot accepts MovJ command.")
+            return True
+
+        self.__dobot.log_warning_msg("out of range.")
+        return False
