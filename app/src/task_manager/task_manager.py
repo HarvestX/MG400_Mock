261256f6b72ca5f7c954a8fe6b2c44ad7c2e8875
Yuki Yamamoto <yuki8mamo10.hu@gmail.com>
2022-08-19 17:28:19 +0900

    task_manager base

--- app/src/task_manager/task_manager.py
+++ app/src/task_manager/task_manager.py
@@ -1,2 +1,17 @@
 """task manager."""
 import collections
+
+
+class TaskManager:
+    """TaskManager"""
+
+    def __init__(self) -> None:
+        self.__que = collections.deque("")
+
+    def stack(self, command: str):
+        """stack"""
+        self.__que.append(command)
+
+    def unstack(self):
+        """unstack"""
+        return self.__que.pop()
