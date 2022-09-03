44b1ea024dc453dea2622fd4c541e2833f9e825c
Yuki Yamamoto <yuki8mamo10.hu@gmail.com>
2022-09-04 04:27:44 +0900

    add test for out of range sets

--- app/tests/utilities/test_kinematics_mg400.py
+++ app/tests/utilities/test_kinematics_mg400.py
@@ -23,95 +23,86 @@ from src.utilities.kinematics_mg400 import (ROUND_DECIMALS, forward_kinematics,
 class TestKinematics(TestCase):
     """Test Kinematics."""
 
-    def test_fk_good(self):
-        """forward kinematics test."""
-        angles_list = [
+    def setUp(self) -> None:
+        self.good_angles = [
             [0., 0., 0., 0., 0., 0],
             [160., 0., 0., 0., 0., 0],
             [-160., 0., 0., 0., 0., 0],
-            [0., -25., 25., 0., 0., 0],
+            [0., -25., -25., 0., 0., 0],
             [0., 35., -25., 0., 0., 0],
             [0., 70., 15., 0., 0., 0],
             [0., 85., 25., 0., 0., 0],
             [0., 85., 105., 0., 0., 0],
             [0., 45., 105., 0., 0., 0],
             [0., -25., 35., 0., 0., 0],
+            [0., 0., 0, 90., 0., 0],
+            [0., 0., 0, -90., 0., 0],
         ]
-        exp_ret_list = [
+        self.good_tool_vec = [
             [284.0, 0.0, 118.0, 0.0, 0.0, 0.0],
             [-266.872704, 97.133721, 118.0, 0.0, 0.0, 0.0],
             [-266.872704, -97.133721, 118.0, 0.0, 0.0, 0.0],
-            [193.645667, 0.0, 27.645667, 0.0, 0.0, 0.0],
+            [193.645667, 0., 175.562059, 0., 0., 0.],
             [367.979739, 0.0, 160.309804, 0.0, 0.0, 0.0],
             [442.483228, 0.0, -42.439808, 0.0, 0.0, 0.0],
             [441.937935, 0.0, -115.705941, 0.0, 0.0, 0.0],
             [238.040739, 0.0, -210.784765, 0.0, 0.0, 0.0],
             [187.450354, 0.0, -102.293333, 0.0, 0.0, 0.0],
             [178.393412, 0.0, 1.227986, 0.0, 0.0, 0.0],
+            [284.0, 0.0, 118.0, 0.0, 0.0, 90.0],
+            [284.0, 0.0, 118.0, 0.0, 0.0, -90.0],
         ]
-        for angles, exp_ret in zip(angles_list, exp_ret_list):
-            solved_flag, ret = forward_kinematics(angles)
+        return super().setUp()
+
+    def test_fk_good(self):
+        """forward kinematics test."""
+        for test_input, exp_ret in zip(self.good_angles, self.good_tool_vec):
+            solved_flag, ret = forward_kinematics(test_input)
             self.assertTrue(solved_flag)
             self.assertIsNone(assert_allclose(
-                ret, exp_ret, atol=10**(ROUND_DECIMALS-1)))
+                ret, exp_ret, atol=10**-(ROUND_DECIMALS-1)))
 
     def test_ik_good(self):
         """inverse kinematics test."""
-        tool_vec_list = [
-            [284., 0., 118., 0., 0., 0.],
-            [-266.872704, 97.133721, 118., 0., 0., 0.],
-            [-266.872704, -97.133721, 118., 0., 0., 0.],
-            [193.645667, 0., 27.645667, 0., 0., 0.],
-            [367.979739, 0., 160.309804, 0., 0., 0.],
-            [442.483228, 0., -42.439808, 0., 0., 0.],
-            [441.937935, 0., -115.705941, 0., 0., 0.],
-            [238.040739, 0., -210.784765, 0., 0., 0.],
-            [187.450354, 0., -102.293333, 0., 0., 0.],
-            [178.393412, 0., 1.227986, 0., 0., 0.],
-        ]
-        exp_ret_list = [
-            [0.0, 0.0, 0.0, 0.0, 0.0, 0],
-            [160.0, 0.0, 0.0, 0.0, 0.0, 0],
-            [-160.0, 0.0, 0.0, 0.0, 0.0, 0],
-            [0.0, -25.0, 25.0, 0.0, 0.0, 0],
-            [0.0, 35.0, -25.0, 0.0, 0.0, 0],
-            [0.0, 70.0, 15.0, 0.0, 0.0, 0],
-            [0.0, 85.0, 25.0, 0.0, 0.0, 0],
-            [0.0, 85.0, 105.0, 0.0, 0.0, 0],
-            [0.0, 45.0, 105.0, 0.0, 0.0, 0],
-            [0.0, -25.0, 35.0, 0.0, 0.0, 0],
-        ]
-        for tool_vec, exp_ret in zip(tool_vec_list, exp_ret_list):
-            solved_flag, ret = inverse_kinematics(tool_vec)
+        for test_input, exp_ret in zip(self.good_tool_vec, self.good_angles):
+            solved_flag, ret = inverse_kinematics(test_input)
             self.assertTrue(solved_flag)
             self.assertIsNone(assert_allclose(
-                ret, exp_ret, atol=10**(ROUND_DECIMALS-1)))
+                ret, exp_ret, atol=10**-(ROUND_DECIMALS-1)))
 
-    # def test_fk_out_of_range(self):
-    #     """forward kinematics test."""
-    #     angles_list = [
-    #         [0]*6,
-    #         [0]*6,
-    #         [0]*6,
-    #         [0]*6,
-    #         [0]*6,
-    #     ]
-    #     for angles in angles_list:
-    #         solved_flag, _ = forward_kinematics(angles)
-    #         self.assertFalse(solved_flag)
+    def test_fk_out_of_range(self):
+        """forward kinematics test."""
+        angles_list = [
+            [180., 0., 0., 0., 0., 0],
+            [-180., 0., 0., 0., 0., 0],
+            [0., -45., 25., 0., 0., 0],
+            [0., 35., -45., 0., 0., 0],
+            [0., 90., 15., 0., 0., 0],
+            [0., 85., 5., 0., 0., 0],
+            [0., 105., 105., 0., 0., 0],
+            [0., 25., 105., 0., 0., 0],
+            [0., -25., 55., 0., 0., 0],
+        ]
+        for angles in angles_list:
+            solved_flag, _ = forward_kinematics(angles)
+            self.assertFalse(solved_flag)
 
-    # def test_ik_out_of_range(self):
-    #     """inverse kinematics test."""
-    #     tool_vec_list = [
-    #         [0]*6,
-    #         [0]*6,
-    #         [0]*6,
-    #         [0]*6,
-    #         [0]*6,
-    #     ]
-    #     for tool_vec in tool_vec_list:
-    #         solved_flag, _ = inverse_kinematics(tool_vec)
-    #         self.assertFalse(solved_flag)
+    def test_ik_out_of_range(self):
+        """inverse kinematics test."""
+        tool_vec_list = [
+            [-266.872704, 90., 118.0, 0.0, 0.0, 0.0],
+            [-266.872704, -90., 118.0, 0.0, 0.0, 0.0],
+            [193.645667, 0., 180., 0., 0., 0.],
+            [367.979739, 0.0, 170., 0.0, 0.0, 0.0],
+            [450., 0.0, -42.439808, 0.0, 0.0, 0.0],
+            [450., 0.0, -115.705941, 0.0, 0.0, 0.0],
+            [230., 0.0, -210.784765, 0.0, 0.0, 0.0],
+            [180., 0.0, -102.293333, 0.0, 0.0, 0.0],
+            [170., 0.0, 1.227986, 0.0, 0.0, 0.0],
+        ]
+        for tool_vec in tool_vec_list:
+            solved_flag, _ = inverse_kinematics(tool_vec)
+            self.assertFalse(solved_flag)
 
 
 if __name__ == '__main__':
